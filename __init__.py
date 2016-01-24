2303412d66ff839f07850b2051381b1b7d0e3dc4
Matthew <m.zadrozny@gmail.com>
2016-01-24 13:57:18 -0500

    Fixed indents (take two)

--- __init__.py
+++ __init__.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 # however, for local development this gets in the way.
 # Figure out how to make this work on either without constanty changing!
 class WebFactionMiddleware(object):
-	def __init__(self, app):
-		self.app = app
-	def __call__(self, environ, start_response):
-		environ['SCRIPT_NAME'] = '/quotes' # '/' for local
-		return self.app(environ, start_response)
+    def __init__(self, app):
+        self.app = app
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = '/quotes' # '/' for local
+        return self.app(environ, start_response)
 
 #app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
 
@@ -26,104 +26,107 @@ app.secret_key = 'churchill'
 
 
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
 
 
 @app.route('/')
 def home():
-	'''Generate a default random quote, plus the theme and author index'''
+    '''Generate a default random quote, plus the theme and author index'''
 
-	d = connect_db()
+    d = connect_db()
 
-	# Select a random quote from the database to present as default on opening
-	rows = list(d.execute('''SELECT COUNT(*) FROM quotes'''))[0][0]
-	n = choice(range(1, rows+1))
-	randomquote = d.execute('''SELECT quote, author 
-								FROM quotes 
-								WHERE id=?''', (n,)).fetchone()
-	randomquote = dict(zip(('quote', 'author'), randomquote))
+    # Select a random quote from the database to present as default on opening
+    rows = list(d.execute('''SELECT COUNT(*) FROM quotes'''))[0][0]
 
-	# Construct index of themes, removing redundancies ('DISTINCT'):
-	cur = d.execute('''SELECT DISTINCT tags FROM topics ORDER BY tags ASC''') 
-	tags = [dict(tag=row[0]) for row in cur.fetchall()]
+    n = choice(range(1, rows+1))
+    
+    randomquote = d.execute('''SELECT quote, author 
+                               FROM quotes 
+                               WHERE id=?''', (n,)).fetchone()
+    
+    randomquote = dict(zip(('quote', 'author'), randomquote))
 
-	# Construct index of authors, removing redundancies:
-	cur = d.execute('''SELECT DISTINCT author_full, author_url 
-		                  FROM authors ORDER BY author_full ASC''') 
-	authors = [dict(author=row[0], url=row[1]) for row in cur.fetchall()]
+    # Construct index of themes, removing redundancies ('DISTINCT'):
+    cur = d.execute('''SELECT DISTINCT tags FROM topics ORDER BY tags ASC''') 
+    tags = [dict(tag=row[0]) for row in cur.fetchall()]
 
-	d.close()
+    # Construct index of authors, removing redundancies:
+    cur = d.execute('''SELECT DISTINCT author_full, author_url 
+                       FROM authors ORDER BY author_full ASC''') 
+    authors = [dict(author=row[0], url=row[1]) for row in cur.fetchall()]
 
-	return render_template('index.html', randomquote=randomquote, tags=tags, authors=authors) 
+    d.close()
+
+    return render_template('index.html', randomquote=randomquote, tags=tags, authors=authors) 
 
 
 @app.route('/_gen_quote')
 def generate_quote():
-	'''Generate a random quote for ajax refresh (ie, next button)'''
-	d = connect_db()
-	cur = d.execute('SELECT quote, author, tags FROM quotes')
+    '''Generate a random quote for ajax refresh (ie, next button)'''
+    d = connect_db()
+    cur = d.execute('SELECT quote, author, tags FROM quotes')
 
-	#Rewrite below as a named tuple, more space efficient, in collections module
-	quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()]
-	randomquote = [random.choice(quotes)]
+    #Rewrite below as a named tuple, more space efficient, in collections module
+    quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()]
+    randomquote = [random.choice(quotes)]
 
-	d.close()
-	return jsonify(randomquote=randomquote)
+    d.close()
+    return jsonify(randomquote=randomquote)
 
 
 @app.route('/_gen_topic/')
 @app.route('/_gen_topic/<name>')
 def generate_topic(name):
-	'''Show the quotes for a given topic'''
-	d = connect_db()
+    '''Show the quotes for a given topic'''
+    d = connect_db()
 
-	cur = d.execute("""SELECT quotes.quote, quotes.author 
-	                   FROM quotes 
-	                   INNER JOIN topics 
-	                   ON quotes.Id=topics.Id 
-	                   WHERE topics.tags=(?)""", (name,))
+    cur = d.execute("""SELECT quotes.quote, quotes.author 
+                       FROM quotes 
+                       INNER JOIN topics 
+                       ON quotes.Id=topics.Id 
+                       WHERE topics.tags=(?)""", (name,))
 
-	quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()]
+    quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()]
 
-	d.close()
-	return jsonify(quotes=quotes)
+    d.close()
+    return jsonify(quotes=quotes)
 
 
 @app.route('/_gen_author/')
 @app.route('/_gen_author/<name>')
 def generate_author(name):
-	'''Show quotes from a given author'''
-	d = connect_db()
+    '''Show quotes from a given author'''
+    d = connect_db()
 
-	cur = d.execute("""SELECT quotes.quote, quotes.author 
-					   FROM quotes 
-					   INNER JOIN authors 
-					   ON quotes.Id=authors.Id 
-					   WHERE authors.author_url=(?)""", (name,)) #
+    cur = d.execute("""SELECT quotes.quote, quotes.author 
+    FROM quotes 
+    INNER JOIN authors 
+    ON quotes.Id=authors.Id 
+    WHERE authors.author_url=(?)""", (name,)) #
 
-	# Data structure of below should prob be changed
-	quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()] 
+    # Data structure of below should prob be changed
+    quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()] 
 
-	d.close()
-	return jsonify(quotes=quotes)
+    d.close()
+    return jsonify(quotes=quotes)
 
 
 @app.route('/_gen_author_list/')
 def generate_author_list():
-	'''Generate the index of authors'''
-	g = connect_db()
+    '''Generate the index of authors'''
+    g = connect_db()
 
-	cur = d.execute('''SELECT DISTINCT author_full, author_url 
-					   FROM authors 
-					   ORDER BY author_full ASC''') 
+    cur = d.execute('''SELECT DISTINCT author_full, author_url 
+                       FROM authors 
+                       ORDER BY author_full ASC''') 
 
-	authors = [dict(author=row[0], url=row[1]) for row in cur.fetchall()]
+    authors = [dict(author=row[0], url=row[1]) for row in cur.fetchall()]
 
-	d.close()
+    d.close()
 
-	return jsonify(authors=authors)
+    return jsonify(authors=authors)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
 
