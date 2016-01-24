449ef98587cd5dea75d8643b4612015cedea0488
Matthew <m.zadrozny@gmail.com>
2016-01-24 13:51:43 -0500

    Fixed indents

--- __init__.py
+++ __init__.py
@@ -9,19 +9,19 @@ app = Flask(__name__)
 # however, for local development this gets in the way.
 # Figure out how to make this work on either without constanty changing!
 class WebFactionMiddleware(object):
-    def __init__(self, app):
-        self.app = app
-    def __call__(self, environ, start_response):
-        environ['SCRIPT_NAME'] = '/quotes' # '/' for local
-        return self.app(environ, start_response)
+	def __init__(self, app):
+		self.app = app
+	def __call__(self, environ, start_response):
+		environ['SCRIPT_NAME'] = '/quotes' # '/' for local
+		return self.app(environ, start_response)
 
 #app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
 
+
+
 DATABASE = 'quotations.db'
-
 app.config.from_object(__name__)
-
-app.secret_key = 'boo'
+app.secret_key = 'churchill'
 
 
 
@@ -32,9 +32,9 @@ def connect_db():
 @app.route('/')
 def home():
 	'''Generate a default random quote, plus the theme and author index'''
-	
+
 	d = connect_db()
-	
+
 	# Select a random quote from the database to present as default on opening
 	rows = list(d.execute('''SELECT COUNT(*) FROM quotes'''))[0][0]
 	n = choice(range(1, rows+1))
@@ -42,7 +42,7 @@ def home():
 								FROM quotes 
 								WHERE id=?''', (n,)).fetchone()
 	randomquote = dict(zip(('quote', 'author'), randomquote))
-	
+
 	# Construct index of themes, removing redundancies ('DISTINCT'):
 	cur = d.execute('''SELECT DISTINCT tags FROM topics ORDER BY tags ASC''') 
 	tags = [dict(tag=row[0]) for row in cur.fetchall()]
@@ -51,7 +51,7 @@ def home():
 	cur = d.execute('''SELECT DISTINCT author_full, author_url 
 		                  FROM authors ORDER BY author_full ASC''') 
 	authors = [dict(author=row[0], url=row[1]) for row in cur.fetchall()]
-	
+
 	d.close()
 
 	return render_template('index.html', randomquote=randomquote, tags=tags, authors=authors) 
@@ -62,11 +62,11 @@ def generate_quote():
 	'''Generate a random quote for ajax refresh (ie, next button)'''
 	d = connect_db()
 	cur = d.execute('SELECT quote, author, tags FROM quotes')
-	
+
 	#Rewrite below as a named tuple, more space efficient, in collections module
 	quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()]
 	randomquote = [random.choice(quotes)]
-	
+
 	d.close()
 	return jsonify(randomquote=randomquote)
 
@@ -78,13 +78,13 @@ def generate_topic(name):
 	d = connect_db()
 
 	cur = d.execute("""SELECT quotes.quote, quotes.author 
-					   FROM quotes 
-					   INNER JOIN topics 
-					   ON quotes.Id=topics.Id 
-					   WHERE topics.tags=(?)""", (name,))
+	                   FROM quotes 
+	                   INNER JOIN topics 
+	                   ON quotes.Id=topics.Id 
+	                   WHERE topics.tags=(?)""", (name,))
 
 	quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()]
-	
+
 	d.close()
 	return jsonify(quotes=quotes)
 
@@ -96,14 +96,14 @@ def generate_author(name):
 	d = connect_db()
 
 	cur = d.execute("""SELECT quotes.quote, quotes.author 
-					FROM quotes 
-					INNER JOIN authors 
-					ON quotes.Id=authors.Id 
-					WHERE authors.author_url=(?)""", (name,)) #
-	
+					   FROM quotes 
+					   INNER JOIN authors 
+					   ON quotes.Id=authors.Id 
+					   WHERE authors.author_url=(?)""", (name,)) #
+
 	# Data structure of below should prob be changed
 	quotes = [dict(quote=row[0], author=row[1]) for row in cur.fetchall()] 
-	
+
 	d.close()
 	return jsonify(quotes=quotes)
 
@@ -112,13 +112,13 @@ def generate_author(name):
 def generate_author_list():
 	'''Generate the index of authors'''
 	g = connect_db()
-	
+
 	cur = d.execute('''SELECT DISTINCT author_full, author_url 
 					   FROM authors 
-				       ORDER BY author_full ASC''') 
+					   ORDER BY author_full ASC''') 
 
 	authors = [dict(author=row[0], url=row[1]) for row in cur.fetchall()]
-	
+
 	d.close()
 
 	return jsonify(authors=authors)
